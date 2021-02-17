fb6241f61018d310948ac4bced8b49b88d089f58
Augustus Murphy <gusbmurphy@gmail.com>
2021-02-17 16:23:16 -0600

    Create app-styles

---
+++ src/components/app-styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+import colors from './colors';
+
+const appStyles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+});
+
+export default appStyles;
