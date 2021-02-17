87ec9b85ae7ccbbe88529b3c9365fdb91f3ec9d4
Augustus Murphy <gusbmurphy@gmail.com>
2021-02-17 16:24:15 -0600

    Create QuickSessionButton

--- src/components/home-screen/QuickSessionButton.tsx
+++ src/components/home-screen/QuickSessionButton.tsx
@@ -1,28 +1,27 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {Pressable} from 'react-native';
+import {Pressable, PressableProps} from 'react-native';
 import {StyleSheet, Text} from 'react-native';
+import colors from '../colors';
+import appStyles from '../app-styles';
 
-const QuickSessionButton = () => {
-  const styles = StyleSheet.create({
-    button: {
-      backgroundColor: '#327495',
-      borderRadius: 20,
-      padding: 10,
-      elevation: 2,
-    },
-    text: {
-      color: 'white',
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-  });
-
+const QuickSessionButton = (props: PressableProps) => {
   return (
-    <Pressable style={styles.button} accessibilityLabel="Begin a Quick Session">
+    <Pressable
+      style={appStyles.button}
+      accessibilityLabel="Begin a Quick Session"
+      {...props}>
       <Text style={styles.text}>Quick Session</Text>
     </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default QuickSessionButton;
