87b1e40043966fd75ecea6b8d7dac868983768cd
Augustus Murphy <gusbmurphy@gmail.com>
2021-02-19 09:32:39 -0600

    Add QuickSessionModal test

---
+++ src/components/home-screen/quick-session-modal/QuickSessionModal.test.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+
+describe('Quick Session Modal', () => {
+  test.todo('at first shows buttons to select a focus area');
+  test.todo('after selecting one, reveal a "Next" button');
+  test.todo(
+    'after pressing the next button, a view is presented showing exercise groups',
+  );
+  test.todo('displays the "Next" button after choosing an exercise group');
+  test.todo(
+    'after pressing the "Next" button, presesnts text inputs for a number of hours and minutes',
+  );
+  test.todo('again, displays the "Next" button');
+  test.todo(
+    'presents a summary of the generated session after the "Next" button is pressed',
+  );
+});
