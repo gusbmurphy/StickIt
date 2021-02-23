ea861900c8d9ff97721a40ba3916c5aca789f59f
Augustus Murphy <gusbmurphy@gmail.com>
2021-02-23 17:45:15 -0600

    Configure Jest setup for autocomplete

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,3 @@
-import '@testing-library/jest-native/extend-expect';
-
 // eslint-disable-next-line no-undef
 jest.mock('react-native-gesture-handler', () => {
   const View = require('react-native/Libraries/Components/View/View');
