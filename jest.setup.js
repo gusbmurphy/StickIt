67cf90b6838e6353aa658ea8b3e1789fb53ce984
Augustus Murphy <gusbmurphy@gmail.com>
2021-02-23 17:35:10 -0600

    Add further Jest configuration

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,5 @@
+import '@testing-library/jest-native/extend-expect';
+
 // eslint-disable-next-line no-undef
 jest.mock('react-native-gesture-handler', () => {
   const View = require('react-native/Libraries/Components/View/View');
