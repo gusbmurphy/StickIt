07e2377ab6a668952e57aa848c2adbff3f10f5ec
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-01 17:29:39 -0600

    Update default-areas.ts

--- src/util/default-areas.ts
+++ src/util/default-areas.ts
@@ -1,10 +1,25 @@
-import {FocusArea} from '../types';
+import {ExerciseGroup, FocusArea} from '../types';
 
 const areas = [
-  new FocusArea('Speed & Agility'),
-  new FocusArea('Creativity & Improvisation'),
-  new FocusArea('Style & Vocabulary'),
-  new FocusArea('Precision & Timekeeping'),
+  new FocusArea('Speed & Agility', [
+    new ExerciseGroup('Hand Speed'),
+    new ExerciseGroup('Foot Speed'),
+    new ExerciseGroup('Rudiments'),
+  ]),
+  new FocusArea('Creativity & Improvisation', [
+    new ExerciseGroup('Using Space'),
+    new ExerciseGroup('Melodic Embelishment'),
+  ]),
+  new FocusArea('Style & Vocabulary', [
+    new ExerciseGroup('Max Roach Patterns'),
+    new ExerciseGroup('Verbs'),
+    new ExerciseGroup('Nouns'),
+  ]),
+  new FocusArea('Precision & Timekeeping', [
+    new ExerciseGroup('Playing Slow'),
+    new ExerciseGroup('Hours & Minutes'),
+    new ExerciseGroup('Military Time'),
+  ]),
 ];
 
 export default areas;
