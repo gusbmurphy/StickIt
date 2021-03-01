0ae2aa2753dd7d3ef0c76c94473fea5de482ac3a
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-01 17:30:05 -0600

    Update StepIndicator with labels

--- src/components/home-screen/StepIndicator.tsx
+++ src/components/home-screen/StepIndicator.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import colors from '../colors';
+import {stepIndicatorA11yLabel, stepIndicatorPipTestId} from '../labels';
 
 const StepIndicator = (props: {currentStep: number; totalSteps: number}) => {
   let pips = [];
 
   for (let i = 1; i <= props.totalSteps; i++) {
-    pips.push(<Pip currentStep={i === props.currentStep} key={i} />);
+    pips.push(
+      <Pip currentStep={i === props.currentStep} key={i} stepNumber={i} />,
+    );
   }
 
-  return <View style={styles.pipView}>{pips}</View>;
+  return (
+    <View
+      style={styles.pipView}
+      accessibilityLabel={stepIndicatorA11yLabel(
+        props.currentStep,
+        props.totalSteps,
+      )}>
+      {pips}
+    </View>
+  );
 };
 
-const Pip = (props: {currentStep: boolean}) => {
-  let style = [
+const Pip = (props: {currentStep: boolean; stepNumber: number}) => {
+  let pipStyles = [
     styles.pip,
     props.currentStep ? styles.activePip : styles.inactivePip,
   ];
 
-  return <View style={style} />;
+  return (
+    <View style={pipStyles} testID={stepIndicatorPipTestId(props.stepNumber)} />
+  );
 };
 
 const styles = StyleSheet.create({
