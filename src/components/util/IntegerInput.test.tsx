70d83c14a311d2aadbf3c3562dbf9dd0b036c148
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-02 17:47:46 -0600

    Create IntegerInput test

---
+++ src/components/util/IntegerInput.test.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import IntegerInput from './IntegerInput';
+
+describe('Integer Input', () => {
+  test('any input that is not a digit is not passed to the onChange function', () => {
+    const inputId = 'inputId';
+    const mockOnChange = jest.fn();
+
+    const {getByTestId} = render(
+      <IntegerInput testID={inputId} onChange={mockOnChange} value={0} />,
+    );
+
+    fireEvent.changeText(getByTestId(inputId), 'abc5');
+    expect(mockOnChange).toBeCalledWith(5);
+  });
+  test("deleting all the characters will pass a '0' to the onChange function", () => {
+    const inputId = 'inputId';
+    const mockOnChange = jest.fn((value) => (x = value));
+
+    const {getByTestId} = render(
+      <IntegerInput testID={inputId} onChange={mockOnChange} value={0} />,
+    );
+
+    fireEvent.changeText(getByTestId(inputId), '');
+    expect(mockOnChange).toBeCalledWith(0);
+  });
+});
