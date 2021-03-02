21d5a03a6046be766e67e7bed9f581f2fde04977
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-02 14:30:10 -0600

    Rename HomeScreen to index

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from './src/components/home-screen/HomeScreen';
+import HomeScreen from './src/components/home-screen';
 
 const Stack = createStackNavigator();
 
