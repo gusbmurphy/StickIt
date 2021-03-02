3544f98c01d0250c63eb860637310993661b8c91
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-02 17:55:14 -0600

    Update quick session summary label

--- src/components/labels.ts
+++ src/components/labels.ts
@@ -5,7 +5,8 @@ export const exerciseGroupA11yLabel = (groupName: string) =>
   `Select ${groupName} exercise group`;
 export const minuteInputA11yLabel = 'Enter number of minutes for session';
 export const hourInputA11yLabel = 'Enter number of hours for session';
-export const generatedQuickSessionSummaryA11yLabel = 'Generated quick session';
+export const generatedQuickSessionSummaryA11yLabel =
+  'Summary of generated quick session';
 export const stepIndicatorA11yLabel = (
   currentStep: number,
   totalSteps: number,
