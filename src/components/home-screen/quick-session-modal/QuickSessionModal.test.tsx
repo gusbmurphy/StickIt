39983e6868e8db11dfa81f8c8c607c5ff9b4d278
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-02 18:49:46 -0600

    Refactor tests with renderAndCompleteUntilStep()

--- src/components/home-screen/quick-session-modal/QuickSessionModal.test.tsx
+++ src/components/home-screen/quick-session-modal/QuickSessionModal.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {fireEvent, render} from '@testing-library/react-native';
-import QuickSessionModal from '.';
+import {fireEvent, render, RenderAPI} from '@testing-library/react-native';
+import QuickSessionModal, {SessionSetupStep} from '.';
 import areas from '../../../util/default-areas';
 import {
   exerciseGroupA11yLabel,
@@ -13,11 +13,76 @@ import {
   nextButtonA11yLabel,
 } from '../../labels';
 import colors from '../../colors';
+import {ExerciseGroup, FocusArea} from '../../../types';
 
 /* Thanks to StackOverflow user jonschlinkert: https://stackoverflow.com/a/7228322/6741328, no way I could've come up with this. */
 function randomIntFromInterval(min: number, max: number) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+interface RenderAndCompleteUntilStepReturnObject
+  extends ReturnType<typeof render> {
+  selectedFocusArea?: FocusArea;
+  selectedExerciseGroup?: ExerciseGroup;
+  selectedMinuteValue?: number;
+  selectedHourValue?: number;
+}
+
+function renderAndCompleteUntilStep(
+  stopStep: SessionSetupStep,
+): RenderAndCompleteUntilStepReturnObject {
+  const helpers = render(<QuickSessionModal areas={areas} />);
+  let returnObject: RenderAndCompleteUntilStepReturnObject = {...helpers};
+  const {getByA11yLabel} = helpers;
+
+  if (stopStep > SessionSetupStep.Area) {
+    const selectedFocusArea = areas[randomIntFromInterval(0, areas.length - 1)];
+    const selectedFocusAreaButton = getByA11yLabel(
+      areaButtonA11yLabel(selectedFocusArea.name),
+    );
+    fireEvent.press(selectedFocusAreaButton);
+    returnObject = {...returnObject, selectedFocusArea};
+
+    let nextButton = getByA11yLabel(nextButtonA11yLabel);
+    fireEvent.press(nextButton);
+
+    if (stopStep > SessionSetupStep.Group) {
+      const selectedExerciseGroup =
+        selectedFocusArea.exerciseGroups[
+          randomIntFromInterval(0, selectedFocusArea.exerciseGroups.length - 1)
+        ];
+      const selectedExerciseGroupButton = getByA11yLabel(
+        exerciseGroupA11yLabel(selectedExerciseGroup.name),
+      );
+      fireEvent.press(selectedExerciseGroupButton);
+      returnObject = {...returnObject, selectedExerciseGroup};
+
+      nextButton = getByA11yLabel(nextButtonA11yLabel);
+      fireEvent.press(nextButton);
+
+      if (stopStep > SessionSetupStep.Time) {
+        const minuteInput = getByA11yLabel(minuteInputA11yLabel);
+        const numOfMinutes = randomIntFromInterval(5, 30);
+        fireEvent.changeText(minuteInput, numOfMinutes.toString());
+
+        const hourInput = getByA11yLabel(hourInputA11yLabel);
+        const numOfHours = randomIntFromInterval(1, 3);
+        fireEvent.changeText(hourInput, numOfHours.toString());
+
+        returnObject = {
+          ...returnObject,
+          selectedMinuteValue: numOfMinutes,
+          selectedHourValue: numOfHours,
+        };
+
+        nextButton = getByA11yLabel(nextButtonA11yLabel);
+        fireEvent.press(nextButton);
+      }
+    }
+  }
+
+  return returnObject;
 }
 
 describe('Quick Session Modal', () => {
@@ -52,18 +117,11 @@ describe('Quick Session Modal', () => {
   });
 
   test('after pressing the next button, the focus are buttons go away, and buttons showing exercise groups are presented', () => {
-    const {getByA11yLabel, queryByA11yLabel} = render(
-      <QuickSessionModal areas={areas} />,
-    );
-    const selectedFocusArea = areas[randomIntFromInterval(0, areas.length - 1)];
-    const selectedFocusAreaButton = getByA11yLabel(
-      areaButtonA11yLabel(selectedFocusArea.name),
-    );
-
-    fireEvent.press(selectedFocusAreaButton);
-
-    const nextButton = getByA11yLabel(nextButtonA11yLabel);
-    fireEvent.press(nextButton);
+    const {
+      queryByA11yLabel,
+      getByA11yLabel,
+      selectedFocusArea,
+    } = renderAndCompleteUntilStep(SessionSetupStep.Group);
 
     const focusAreaButtons = areas.map((area) =>
       queryByA11yLabel(areaButtonA11yLabel(area.name)),
@@ -71,65 +129,35 @@ describe('Quick Session Modal', () => {
 
     focusAreaButtons.forEach((button) => expect(button).toBeFalsy());
 
-    const exerciseGroupButtons = selectedFocusArea.exerciseGroups.map(
+    const exerciseGroupButtons = selectedFocusArea!.exerciseGroups.map(
       (exerciseGroup) =>
         getByA11yLabel(exerciseGroupA11yLabel(exerciseGroup.name)),
     );
 
     expect(exerciseGroupButtons).toHaveLength(
-      selectedFocusArea.exerciseGroups.length,
+      selectedFocusArea!.exerciseGroups.length,
     );
   });
 
   test('the "Next" button is displayed after choosing an exercise group', () => {
-    const {getByA11yLabel} = render(<QuickSessionModal areas={areas} />);
-    const selectedFocusArea = areas[randomIntFromInterval(0, areas.length - 1)];
-    const selectedFocusAreaButton = getByA11yLabel(
-      areaButtonA11yLabel(selectedFocusArea.name),
+    const {getByA11yLabel, selectedFocusArea} = renderAndCompleteUntilStep(
+      SessionSetupStep.Group,
     );
 
-    fireEvent.press(selectedFocusAreaButton);
-
-    let nextButton = getByA11yLabel(nextButtonA11yLabel);
-    fireEvent.press(nextButton);
-
-    const selectedExerciseGroup =
-      selectedFocusArea.exerciseGroups[
-        randomIntFromInterval(0, selectedFocusArea.exerciseGroups.length - 1)
-      ];
+    const selectedExerciseGroup = selectedFocusArea!.exerciseGroups[
+      randomIntFromInterval(0, selectedFocusArea!.exerciseGroups.length - 1)
+    ];
     const selectedExerciseGroupButton = getByA11yLabel(
       exerciseGroupA11yLabel(selectedExerciseGroup.name),
     );
 
     fireEvent.press(selectedExerciseGroupButton);
-    nextButton = getByA11yLabel(nextButtonA11yLabel);
+    const nextButton = getByA11yLabel(nextButtonA11yLabel);
     expect(nextButton).toBeTruthy();
   });
 
   test('after pressing the "Next" button, text inputs for hours and minutes are presented', () => {
-    const {getByA11yLabel} = render(<QuickSessionModal areas={areas} />);
-    const selectedFocusArea = areas[randomIntFromInterval(0, areas.length - 1)];
-    const selectedFocusAreaButton = getByA11yLabel(
-      areaButtonA11yLabel(selectedFocusArea.name),
-    );
-
-    fireEvent.press(selectedFocusAreaButton);
-
-    let nextButton = getByA11yLabel(nextButtonA11yLabel);
-    fireEvent.press(nextButton);
-
-    const selectedExerciseGroup =
-      selectedFocusArea.exerciseGroups[
-        randomIntFromInterval(0, selectedFocusArea.exerciseGroups.length - 1)
-      ];
-    const selectedExerciseGroupButton = getByA11yLabel(
-      exerciseGroupA11yLabel(selectedExerciseGroup.name),
-    );
-
-    fireEvent.press(selectedExerciseGroupButton);
-    nextButton = getByA11yLabel(nextButtonA11yLabel);
-
-    fireEvent.press(nextButton);
+    const {getByA11yLabel} = renderAndCompleteUntilStep(SessionSetupStep.Time);
 
     const minuteInput = getByA11yLabel(minuteInputA11yLabel);
     const hourInput = getByA11yLabel(hourInputA11yLabel);
@@ -139,106 +167,31 @@ describe('Quick Session Modal', () => {
   });
 
   test('after inputing ONLY a number of minutes, the "Next" button is displayed', () => {
-    const {getByA11yLabel} = render(<QuickSessionModal areas={areas} />);
-    const selectedFocusArea = areas[randomIntFromInterval(0, areas.length - 1)];
-    const selectedFocusAreaButton = getByA11yLabel(
-      areaButtonA11yLabel(selectedFocusArea.name),
-    );
-
-    fireEvent.press(selectedFocusAreaButton);
-
-    let nextButton = getByA11yLabel(nextButtonA11yLabel);
-    fireEvent.press(nextButton);
-
-    const selectedExerciseGroup =
-      selectedFocusArea.exerciseGroups[
-        randomIntFromInterval(0, selectedFocusArea.exerciseGroups.length - 1)
-      ];
-    const selectedExerciseGroupButton = getByA11yLabel(
-      exerciseGroupA11yLabel(selectedExerciseGroup.name),
-    );
-
-    fireEvent.press(selectedExerciseGroupButton);
-    nextButton = getByA11yLabel(nextButtonA11yLabel);
-
-    fireEvent.press(nextButton);
+    const {getByA11yLabel} = renderAndCompleteUntilStep(SessionSetupStep.Time);
 
     const minuteInput = getByA11yLabel(minuteInputA11yLabel);
     const inputString = randomIntFromInterval(5, 30).toString();
     fireEvent.changeText(minuteInput, inputString);
 
-    nextButton = getByA11yLabel(nextButtonA11yLabel);
+    const nextButton = getByA11yLabel(nextButtonA11yLabel);
     expect(nextButton).toBeTruthy();
   });
 
   test('after inputing ONLY a number of hours, the "Next" button is displayed', () => {
-    const {getByA11yLabel} = render(<QuickSessionModal areas={areas} />);
-    const selectedFocusArea = areas[randomIntFromInterval(0, areas.length - 1)];
-    const selectedFocusAreaButton = getByA11yLabel(
-      areaButtonA11yLabel(selectedFocusArea.name),
-    );
-
-    fireEvent.press(selectedFocusAreaButton);
-
-    let nextButton = getByA11yLabel(nextButtonA11yLabel);
-    fireEvent.press(nextButton);
-
-    const selectedExerciseGroup =
-      selectedFocusArea.exerciseGroups[
-        randomIntFromInterval(0, selectedFocusArea.exerciseGroups.length - 1)
-      ];
-    const selectedExerciseGroupButton = getByA11yLabel(
-      exerciseGroupA11yLabel(selectedExerciseGroup.name),
-    );
-
-    fireEvent.press(selectedExerciseGroupButton);
-    nextButton = getByA11yLabel(nextButtonA11yLabel);
-
-    fireEvent.press(nextButton);
+    const {getByA11yLabel} = renderAndCompleteUntilStep(SessionSetupStep.Time);
 
     const hourInput = getByA11yLabel(hourInputA11yLabel);
     const inputString = randomIntFromInterval(1, 3).toString();
     fireEvent.changeText(hourInput, inputString);
 
-    nextButton = getByA11yLabel(nextButtonA11yLabel);
+    const nextButton = getByA11yLabel(nextButtonA11yLabel);
     expect(nextButton).toBeTruthy();
   });
 
   test('a summary of the generated session is presented after the "Next" button is pressed', async () => {
-    const {getByA11yLabel} = render(<QuickSessionModal areas={areas} />);
-    const selectedFocusArea = areas[randomIntFromInterval(0, areas.length - 1)];
-    const selectedFocusAreaButton = getByA11yLabel(
-      areaButtonA11yLabel(selectedFocusArea.name),
+    const {getByA11yLabel} = renderAndCompleteUntilStep(
+      SessionSetupStep.Summary,
     );
-
-    fireEvent.press(selectedFocusAreaButton);
-
-    let nextButton = getByA11yLabel(nextButtonA11yLabel);
-    fireEvent.press(nextButton);
-
-    const selectedExerciseGroup =
-      selectedFocusArea.exerciseGroups[
-        randomIntFromInterval(0, selectedFocusArea.exerciseGroups.length - 1)
-      ];
-    const selectedExerciseGroupButton = getByA11yLabel(
-      exerciseGroupA11yLabel(selectedExerciseGroup.name),
-    );
-
-    fireEvent.press(selectedExerciseGroupButton);
-
-    nextButton = getByA11yLabel(nextButtonA11yLabel);
-    fireEvent.press(nextButton);
-
-    const minuteInput = getByA11yLabel(minuteInputA11yLabel);
-    const numOfMinutes = randomIntFromInterval(5, 30);
-    fireEvent.changeText(minuteInput, numOfMinutes.toString());
-
-    const hourInput = getByA11yLabel(hourInputA11yLabel);
-    const numOfHours = randomIntFromInterval(1, 3);
-    fireEvent.changeText(hourInput, numOfHours.toString());
-
-    nextButton = getByA11yLabel(nextButtonA11yLabel);
-    fireEvent.press(nextButton);
 
     const generatedSessionSummary = getByA11yLabel(
       generatedQuickSessionSummaryA11yLabel,
@@ -246,10 +199,12 @@ describe('Quick Session Modal', () => {
     expect(generatedSessionSummary).toBeTruthy();
   });
 
-  test.todo('a number of exercises are presented after the time is set');
+  test.todo(
+    'a number of exercises are presented after the time is set in the summary',
+  );
   test.todo('on the final presentation, there is a "Start" button');
   test.todo(
-    'each exercise has a "reroll" button next to it, when pressed it replaces the exercise',
+    'each exercise in the summary has a "reroll" button next to it, when pressed it replaces the exercise',
   );
   test.todo(
     'when an exercise is rerolled, all requirements previously set are still met',
