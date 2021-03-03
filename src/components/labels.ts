4a99a02ba2d9f1be4ce50104b5169968054a2c0b
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-03 17:03:43 -0600

    Add more quick session summary labels

--- src/components/labels.ts
+++ src/components/labels.ts
@@ -12,3 +12,32 @@ export const stepIndicatorA11yLabel = (
   totalSteps: number,
 ) => `Currently on step ${currentStep} out of ${totalSteps}`;
 export const stepIndicatorPipTestId = (step: number) => `indicator-pip-${step}`;
+export const quickSessionSummaryExerciseA11yLabel = (
+  groupName: string,
+  exerciseName: string,
+  duration: number,
+) => `${exerciseName} from ${groupName} for ${duration} minutes`;
+export const quickSessionSummaryExerciseTestId = 'exercise';
+export const quickSessionStartButtonA11yLabel = 'Begin quick session';
+export const quickSessionSummaryTotalTimeA11yLabel = (
+  minutes?: number,
+  hours?: number,
+): string => {
+  if ((!minutes && !hours) || (minutes === 0 && hours === 0)) {
+    throw new Error('0 minutes and 0 hours passed to label');
+  }
+  if (!hours || hours === 0) {
+    return `Total quick session time is ${minutes} minute${
+      minutes! > 1 ? 's' : ''
+    }`;
+  } else if (!minutes || minutes === 0) {
+    return `Total quick session time is ${hours} hour${hours > 1 ? 's' : ''}`;
+  }
+  return `Total quick session time is ${hours} hour${
+    hours > 1 ? 's' : ''
+  } and ${minutes} minute${minutes > 1 ? 's' : ''}`;
+};
+export const quickSessionSummaryRerollButtonA11yLabel = (
+  exerciseName: string,
+) => `Replace ${exerciseName}`;
+export const quickSessionSummaryRerollButtonTestId = 'reroll-button';
