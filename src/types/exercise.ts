6dfa98f4ae74a0f97425a691dfc047be4c38c294
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-04 14:59:24 -0600

    Add parentGroupId property to exercise class

--- src/types/exercise.ts
+++ src/types/exercise.ts
@@ -3,9 +3,13 @@ import {v4 as uuid} from 'uuid';
 export class Exercise {
   name: string;
   id: string;
+  parentGroupId?: string;
 
-  constructor(name: string) {
+  constructor(name: string, parentGroupId?: string) {
     this.name = name;
     this.id = uuid();
+    if (parentGroupId) {
+      this.parentGroupId = parentGroupId;
+    }
   }
 }
