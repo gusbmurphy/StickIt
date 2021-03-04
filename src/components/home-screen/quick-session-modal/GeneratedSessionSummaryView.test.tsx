2356fb565253f1ebfe650652b908fb9749e7e986
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-04 15:52:31 -0600

    Create generated session summary test

---
+++ src/components/home-screen/quick-session-modal/GeneratedSessionSummaryView.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+describe('Generated Quick Session Summary View', () => {
+  test.todo('given a session summary, an exercise view for each exercise');
+  test.todo(
+    'the quick session summary displays the total amount of time for the session',
+  );
+  test.todo(
+    'each exercise in the summary has a "reroll" button next to it, when pressed it replaces the exercise',
+  );
+  test.todo(
+    'when an exercise is rerolled, all requirements previously set are still met',
+  );
+});
+
+describe('Quick Session Summary Exercise View', () => {
+  test.todo('given an exercise and a duration, displays the exercise name, its group, and its duration');
+  test.todo('there is an information button, that when pressed brings up an Exercise Descripion Modal');
+});
