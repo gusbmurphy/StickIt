3cd7c60e4a41b8681502cd143b959368390a63a5
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-08 15:34:55 -0500

    Move summary total label into QS summary view

--- src/components/home-screen/quick-session-modal/GeneratedSessionSummaryView.tsx
+++ src/components/home-screen/quick-session-modal/GeneratedSessionSummaryView.tsx
@@ -28,6 +28,24 @@ export const SummaryExerciseView = (props: {
 
 export const generatedQuickSessionSummaryA11yLabel =
   'Summary of generated quick session';
+export const summaryTotalTimeA11yLabel = (
+  minutes?: number,
+  hours?: number,
+): string => {
+  if ((!minutes && !hours) || (minutes === 0 && hours === 0)) {
+    throw new Error('0 minutes and 0 hours passed to label');
+  }
+  if (!hours || hours === 0) {
+    return `Total quick session time is ${minutes} minute${
+      minutes! > 1 ? 's' : ''
+    }`;
+  } else if (!minutes || minutes === 0) {
+    return `Total quick session time is ${hours} hour${hours > 1 ? 's' : ''}`;
+  }
+  return `Total quick session time is ${hours} hour${
+    hours > 1 ? 's' : ''
+  } and ${minutes} minute${minutes > 1 ? 's' : ''}`;
+};
 
 export const summaryExerciseTestId = 'exercise';
 export const summaryExerciseDurationTestId = 'exercise-duration';
--- src/components/home-screen/quick-session-modal/index.tsx
+++ src/components/home-screen/quick-session-modal/index.tsx
@@ -255,28 +255,6 @@ const styles = StyleSheet.create({
 
 export const startButtonA11yLabel = 'Begin quick session';
 
-export const summaryTotalTimeA11yLabel = (
-  minutes?: number,
-  hours?: number,
-): string => {
-  if ((!minutes && !hours) || (minutes === 0 && hours === 0)) {
-    throw new Error('0 minutes and 0 hours passed to label');
-  }
-  if (!hours || hours === 0) {
-    return `Total quick session time is ${minutes} minute${
-      minutes! > 1 ? 's' : ''
-    }`;
-  } else if (!minutes || minutes === 0) {
-    return `Total quick session time is ${hours} hour${hours > 1 ? 's' : ''}`;
-  }
-  return `Total quick session time is ${hours} hour${
-    hours > 1 ? 's' : ''
-  } and ${minutes} minute${minutes > 1 ? 's' : ''}`;
-};
-
-export const exerciseRerollButtonA11yLabel = (exerciseName: string) =>
-  `Replace ${exerciseName}`;
-
 export const exerciseRerollButtonTestId = 'reroll-button';
 export const nextButtonA11yLabel = 'Proceed to next choice';
 export const minuteInputA11yLabel = 'Enter number of minutes for session';
