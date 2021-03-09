25d4e05912420e3001561f7c35cbcaba73209b8d
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-09 16:34:56 -0500

    Add styling to QS summary exercise view

--- src/components/home-screen/quick-session-modal/SummaryExerciseView.tsx
+++ src/components/home-screen/quick-session-modal/SummaryExerciseView.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {Exercise} from '../../../types';
+import colors from '../../colors';
 import {
   summaryExerciseTestId,
   quickSessionSummaryExerciseA11yLabel,
@@ -18,9 +19,46 @@ export const SummaryExerciseView = (props: {
       props.groupName,
       props.exercise.name,
       props.numberOfMinutes,
-    )}>
-    <Text>{props.exercise.name}</Text>
-    <Text>{props.groupName}</Text>
-    <Text testID={summaryExerciseDurationTestId}>{props.numberOfMinutes}</Text>
+    )}
+    style={styles.mainContainer}>
+    <InfoButton />
+    <View style={styles.nameDurationContainer}>
+      <View>
+        <Text style={styles.groupText}>{props.groupName}</Text>
+        <Text style={styles.largeText}>{props.exercise.name}</Text>
+      </View>
+      <Text style={styles.largeText} testID={summaryExerciseDurationTestId}>
+        {props.numberOfMinutes}
+      </Text>
+    </View>
   </View>
 );
+
+const InfoButton = () => <Text style={styles.infoButton}>I</Text>;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: colors.secondary,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 8,
+    paddingVertical: 8,
+  },
+  nameDurationContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingRight: 12,
+  },
+  largeText: {
+    fontSize: 18,
+  },
+  groupText: {
+    fontSize: 12,
+  },
+  infoButton: {
+    marginHorizontal: 8,
+  },
+});
