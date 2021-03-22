9264f6072c9d0ea06c9139a6fa6a2e6f01b7da68
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-22 14:37:09 -0400

    Sketch out tests for Session Screen

--- src/components/session-screen/SessionScreen.test.tsx
+++ src/components/session-screen/SessionScreen.test.tsx
@@ -1,3 +1,20 @@
 describe('Session Screen', () => {
-
+  test.todo('there is an ExerciseInfo component');
+  test.todo('there is an ExerciseClock component');
+  test.todo('there is a Metronome component');
+  test.todo(
+    'when the exercise has finished, an "Add Note" button is displayed',
+  );
+  test.todo(
+    'the "Add Note" button is not displayed before the exercise has finished',
+  );
+  test.todo('pressing the "Add Note" button brings up a modal to add a note');
+  test.todo('when the exercise is finished, a "Next" button is displayed');
+  test.todo(
+    'the "Next" button is not displayed before the exercise is finished',
+  );
+  test.todo('pressing the "Next" button navigates to the next exercise');
+  test.todo(
+    'there are progress pips showing the correct number of exercises in the session, and the correct current exercise',
+  );
 });
