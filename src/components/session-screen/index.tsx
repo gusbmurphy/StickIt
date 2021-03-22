ec1a93ad4ae54741f187edc4c0f440b6ba9e8b9e
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-22 15:46:48 -0400

    Add dummy component for SessionScreen

--- src/components/session-screen/index.tsx
+++ src/components/session-screen/index.tsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const SessionScreen = () => <Text>Session Screen</Text>;
+
+export default SessionScreen;
+
 export const exerciseNameA11yLabel = 'Current exercise';
 export const exerciseFocusGroupA11yLavel = 'Exercise focus group';
 export const exerciseInfoA11yLabel = 'Press to show more info on exercise';
