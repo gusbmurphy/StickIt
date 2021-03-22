a0ece5b6be4aa4063f5465f7754034bb3fe83b72
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-22 14:36:28 -0400

    Add accessibility labels for Session Screen

---
+++ src/components/session-screen/index.tsx
@@ -0,0 +1,5 @@
+export const exerciseNameA11yLabel = 'Current exercise';
+export const exerciseFocusGroupA11yLavel = 'Exercise focus group';
+export const exerciseInfoA11yLabel = 'Press to show more info on exercise';
+export const addNoteButtonA11yLabel = 'Press to add note for exercise';
+export const nextButtonA11yLabel = 'Press to go to next exercise';
