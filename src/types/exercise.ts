5a6793c3fa0fe6c0a3e97abff455ab5761751ca1
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-23 10:44:10 -0400

    Add parent group name to Exercise class

--- src/types/exercise.ts
+++ src/types/exercise.ts
@@ -3,13 +3,15 @@ import {v4 as uuid} from 'uuid';
 export class Exercise {
   name: string;
   id: string;
+  parentGroupName?: string;
   parentGroupId?: string;
 
-  constructor(name: string, parentGroupId?: string) {
+  constructor(name: string, parentGroupId?: string, parentGroupName?: string) {
     this.name = name;
     this.id = uuid();
     if (parentGroupId) {
       this.parentGroupId = parentGroupId;
+      this.parentGroupName = parentGroupName;
     }
   }
 }
