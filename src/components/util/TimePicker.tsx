f0abd2a40ca93c16ce178703f79bdd41da40e339
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-23 15:04:01 -0400

    Add TimePicker.tsx

---
+++ src/components/util/TimePicker.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const TimePicker = () => {
+  return (
+    <>
+      <Text>Time Picker</Text>
+    </>
+  );
+};
+
+export default TimePicker;
