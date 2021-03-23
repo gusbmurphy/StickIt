36b2c574d5d474c1ba48deb3df955a2496c06c25
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-23 10:37:17 -0400

    Rename exercise group a11y label

--- src/components/session-screen/index.tsx
+++ src/components/session-screen/index.tsx
@@ -6,7 +6,7 @@ const SessionScreen = () => <Text>Session Screen</Text>;
 export default SessionScreen;
 
 export const exerciseNameA11yLabel = 'Current exercise';
-export const exerciseFocusGroupA11yLavel = 'Exercise focus group';
+export const exerciseGroupA11yLavel = 'Exercise focus group';
 export const exerciseInfoA11yLabel = 'Press to show more info on exercise';
 export const addNoteButtonA11yLabel = 'Press to add note for exercise';
 export const nextButtonA11yLabel = 'Press to go to next exercise';
