024d48e8a727f20e63b5ac0d832d1ba2df9ddaf2
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-23 13:49:53 -0400

    Add ExerciseClock.tsx

---
+++ src/components/session-screen/ExerciseClock.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ExerciseClock = () => {
+  return (
+    <View>
+      <Text>Exercise Clock</Text>
+    </View>
+  );
+};
+
+export default ExerciseClock;
+
+export const clockTestId = 'exercise-clock';
