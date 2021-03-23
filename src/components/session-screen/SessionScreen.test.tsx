9b51adfe40c3d267e5f81d8c11abe667153c68d4
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-23 10:58:35 -0400

    Write out first Session Screen test

--- src/components/session-screen/SessionScreen.test.tsx
+++ src/components/session-screen/SessionScreen.test.tsx
@@ -1,5 +1,29 @@
+import React from 'react';
+import {render, within} from '@testing-library/react-native';
+import SessionScreen, {exerciseNameA11yLabel, exerciseGroupA11yLabel} from '.';
+import {generateSession} from '../../util/generate-data';
+
 describe('Session Screen', () => {
-  test.todo('there is an ExerciseInfo component');
+  test("the current exercise's name and focus group is displayed", () => {
+    const session = generateSession();
+    const {queryByA11yLabel} = render(<SessionScreen session={session} />);
+
+    const name = queryByA11yLabel(exerciseNameA11yLabel);
+    expect(name).toBeTruthy();
+
+    const group = queryByA11yLabel(exerciseGroupA11yLabel);
+    expect(group).toBeTruthy();
+
+    const firstName = session.exercises[0].name;
+    const firstGroupName = session.exercises[0].parentGroupName!;
+    expect(within(name?.instance).getByText(firstName)).toBeTruthy();
+    expect(within(group?.instance).getByText(firstGroupName)).toBeTruthy();
+  });
+
+  test.todo('there is an "Info" button');
+  test.todo(
+    'pressing the info button brings up a modal with more info on the current exercise',
+  );
   test.todo('there is an ExerciseClock component');
   test.todo('there is a Metronome component');
   test.todo(
