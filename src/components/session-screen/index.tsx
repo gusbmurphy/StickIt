c3197f5b108fae1e35d62b7f714d2969e8d97a80
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-24 17:20:32 -0400

    Fill out more of SessionScreen

--- src/components/session-screen/index.tsx
+++ src/components/session-screen/index.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Button, TouchableOpacity} from 'react-native';
+import {ExerciseSession} from '../../types/exercise-session';
+import ExerciseClock from './ExerciseClock';
+import Metronome from './Metronome';
 
-const SessionScreen = () => <Text>Session Screen</Text>;
+const SessionScreen = ({session}: {session: ExerciseSession}) => {
+  const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
+
+  return (
+    <View>
+      <TouchableOpacity accessibilityLabel={exerciseInfoButtonA11yLabel}>
+        Info
+      </TouchableOpacity>
+      <Text accessibilityLabel={exerciseNameA11yLabel}>
+        {session.exercises[currentExerciseIndex].name}
+      </Text>
+      <Text accessibilityLabel={exerciseGroupA11yLabel}>
+        {session.exercises[currentExerciseIndex].parentGroupName}
+      </Text>
+      <ExerciseClock />
+      <Metronome />
+    </View>
+  );
+};
 
 export default SessionScreen;
 
 export const exerciseNameA11yLabel = 'Current exercise';
-export const exerciseGroupA11yLavel = 'Exercise focus group';
-export const exerciseInfoA11yLabel = 'Press to show more info on exercise';
+export const exerciseGroupA11yLabel = 'Exercise focus group';
+export const exerciseInfoButtonA11yLabel =
+  'Press to show more info on exercise';
 export const addNoteButtonA11yLabel = 'Press to add note for exercise';
 export const nextButtonA11yLabel = 'Press to go to next exercise';
