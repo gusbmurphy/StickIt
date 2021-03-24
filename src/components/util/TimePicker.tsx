460d057d226f058c622f57a9375f50ee333d100a
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-24 10:52:07 -0400

    Add PickerIOS components to TimePicker

--- src/components/util/TimePicker.tsx
+++ src/components/util/TimePicker.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View} from 'react-native';
+import {PickerIOS} from '@react-native-picker/picker';
 
-const TimePicker = () => {
+const hourItems = [...Array(5).keys()].map((value) => (
+  <PickerIOS.Item label={value.toString()} value={value} key={value} />
+));
+
+const minuteItems = [...Array(61).keys()]
+  .filter((value) => value % 5 === 0)
+  .map((value) => (
+    <PickerIOS.Item label={value.toString()} value={value} key={value} />
+  ));
+
+const TimePicker = ({
+  minutes,
+  setMinutes,
+  hours,
+  setHours,
+}: {
+  minutes: number;
+  setMinutes: React.Dispatch<React.SetStateAction<number>>;
+  hours: number;
+  setHours: React.Dispatch<React.SetStateAction<number>>;
+}) => {
   return (
-    <>
-      <Text>Time Picker</Text>
-    </>
+    <View>
+      <PickerIOS
+        selectedValue={hours}
+        onValueChange={(value) => setHours(parseInt(value.toString(), 10))}>
+        {hourItems}
+      </PickerIOS>
+      <PickerIOS
+        selectedValue={minutes}
+        onValueChange={(value) => setMinutes(parseInt(value.toString(), 10))}>
+        {minuteItems}
+      </PickerIOS>
+    </View>
   );
 };
 
