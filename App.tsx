69f7eecf9218c23d9d66980a27c1d9ccbe17ec03
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-25 16:50:14 -0400

    Add session screen to navigation

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './src/components/home-screen';
+import SessionScreen from './src/components/session-screen';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ const App = () => {
       <SafeAreaProvider>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Session" component={SessionScreen} />
         </Stack.Navigator>
       </SafeAreaProvider>
     </NavigationContainer>
