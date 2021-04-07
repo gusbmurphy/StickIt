74d856b813c37c8672f74c593362046b58e180f7
Augustus Murphy <gusbmurphy@gmail.com>
2021-04-07 13:35:48 -0400

    Add more labels for Exercise Clock

--- src/components/session-screen/ExerciseClock.tsx
+++ src/components/session-screen/ExerciseClock.tsx
@@ -12,3 +12,12 @@ const ExerciseClock = () => {
 export default ExerciseClock;
 
 export const clockTestId = 'exercise-clock';
+export const currentTimeA11yLabel = (minutes: Number, seconds: Number) => {
+  let string = '';
+  if (minutes > 0) {
+    string += `${minutes} minutes and `;
+  }
+  string += `${seconds} seconds remaining`;
+  return string;
+};
+export const startButtonA11yLabel = 'Begin exercise clock';
