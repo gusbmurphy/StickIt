20f3beff4b696f185abb09013092f1dced1eb126
Augustus Murphy <gusbmurphy@gmail.com>
2021-04-19 17:09:12 -0400

    Move QSM test render helper to own file

---
+++ src/components/home-screen/quick-session-modal/QuickSessionModal.test.util.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import {render, fireEvent, within, act} from '@testing-library/react-native';
+import {FocusArea, ExerciseGroup} from '../../../types';
+import QuickSessionModal, {SessionSetupStep, nextButtonA11yLabel} from '.';
+import {generateFocusAreas} from '../../../util/generate-data';
+import {randomIntFromInterval} from '../../../util/random-int-from-interval';
+import {areaButtonA11yLabel} from './AreaButton';
+import {exerciseGroupA11yLabel} from './ExerciseGroupButton';
+import {
+  minutePickerA11yLabel,
+  minutePickerValues,
+  hourPickerA11yLabel,
+  hourPickerValues,
+} from '../../util/TimePicker';
+
+interface RenderAndCompleteUntilStepReturnObject
+  extends ReturnType<typeof render> {
+  selectedFocusArea?: FocusArea;
+  selectedExerciseGroup?: ExerciseGroup;
+  selectedMinuteValue?: number;
+  selectedHourValue?: number;
+  onRequestNavigateToSession?: ReturnType<typeof jest.fn>;
+  onRequestCloseSelfNoPlatform?: ReturnType<typeof jest.fn>;
+}
+
+export function renderAndCompleteUntilStep(
+  stopStep: SessionSetupStep,
+): RenderAndCompleteUntilStepReturnObject {
+  const areas = generateFocusAreas(randomIntFromInterval(3, 6));
+  const onRequestNavigateToSession = jest.fn();
+  const onRequestCloseSelfNoPlatform = jest.fn();
+  const helpers = render(
+    <QuickSessionModal
+      areas={areas}
+      onRequestNavigateToSession={onRequestNavigateToSession}
+      onRequestCloseSelfNoPlatform={onRequestCloseSelfNoPlatform}
+    />,
+  );
+  let returnObject: RenderAndCompleteUntilStepReturnObject = {
+    ...helpers,
+    onRequestNavigateToSession,
+    onRequestCloseSelfNoPlatform,
+  };
+  const {getByA11yLabel} = helpers;
+
+  if (stopStep > SessionSetupStep.Area) {
+    const selectedFocusArea = areas[randomIntFromInterval(0, areas.length - 1)];
+    const selectedFocusAreaButton = getByA11yLabel(
+      areaButtonA11yLabel(selectedFocusArea.name),
+    );
+    fireEvent.press(selectedFocusAreaButton);
+    returnObject = {...returnObject, selectedFocusArea};
+
+    let nextButton = getByA11yLabel(nextButtonA11yLabel);
+    fireEvent.press(nextButton);
+
+    if (stopStep > SessionSetupStep.Group) {
+      const selectedExerciseGroup =
+        selectedFocusArea.exerciseGroups[
+          randomIntFromInterval(0, selectedFocusArea.exerciseGroups.length - 1)
+        ];
+      const selectedExerciseGroupButton = getByA11yLabel(
+        exerciseGroupA11yLabel(selectedExerciseGroup.name),
+      );
+      fireEvent.press(selectedExerciseGroupButton);
+      returnObject = {...returnObject, selectedExerciseGroup};
+
+      nextButton = getByA11yLabel(nextButtonA11yLabel);
+      fireEvent.press(nextButton);
+
+      if (stopStep > SessionSetupStep.Time) {
+        // TODO: with the new picker, setting these values like this no longer works
+        const minuteInput = getByA11yLabel(minutePickerA11yLabel);
+        const numOfMinutes =
+          minutePickerValues[
+            randomIntFromInterval(0, minutePickerValues.length)
+          ];
+        const selectedMinuteInput = within(minuteInput).getByText(
+          numOfMinutes.toString(),
+        );
+        act(() => fireEvent.press(selectedMinuteInput));
+
+        const hourInput = getByA11yLabel(hourPickerA11yLabel);
+        const numOfHours =
+          hourPickerValues[randomIntFromInterval(0, hourPickerValues.length)];
+        const selectedHourInput = within(hourInput).getByText(
+          numOfHours.toString(),
+        );
+        act(() => fireEvent.press(selectedHourInput));
+
+        returnObject = {
+          ...returnObject,
+          selectedMinuteValue: numOfMinutes,
+          selectedHourValue: numOfHours,
+        };
+
+        nextButton = getByA11yLabel(nextButtonA11yLabel);
+        act(() => fireEvent.press(nextButton));
+      }
+    }
+  }
+
+  return returnObject;
+}
