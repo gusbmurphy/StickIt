5632623765ae2a91bc30c2000c1791c694f9da55
Augustus Murphy <gusbmurphy@gmail.com>
2021-04-19 17:08:15 -0400

    Updated ESLint config to account for Jest in "test.util" files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  overrides: [
+    {
+      files: ['**/*.test.util.{ts,tsx}'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
