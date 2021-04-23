d0c1e408182f93574605612a1a12bf97df1bb757
Augustus Murphy <gusbmurphy@gmail.com>
2021-04-23 16:41:34 -0400

    Begin fleshing out visuals of metronome

--- src/components/session-screen/Metronome.tsx
+++ src/components/session-screen/Metronome.tsx
@@ -1,13 +1,82 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {colors} from '../../styles';
+import Text from '../../styles/components/StyledText';
 
-const Metronome = () => {
+const OptionButton = ({
+  name,
+  placeholder,
+}: {
+  name: string;
+  placeholder: string;
+}) => {
   return (
-    <View testID={metronomeTestId}>
+    <View style={styles.optionButton}>
+      <Text>{name.toUpperCase()} </Text>
+      <Text>{placeholder}</Text>
+    </View>
+  );
+};
+
+const MainPart = () => {
+  return (
+    <View style={styles.mainPartContainer}>
       <Text>Metronome</Text>
     </View>
   );
 };
+
+const Metronome = () => {
+  return (
+    <View style={styles.mainContainer} testID={metronomeTestId}>
+      <View style={styles.optionColumn}>
+        <OptionButton name="meter" placeholder="3/4" />
+        <OptionButton name="muting" placeholder="off" />
+      </View>
+      <MainPart />
+      <View style={styles.optionColumn}>
+        <OptionButton name="sounds" placeholder="Woodblock" />
+        <View style={styles.optionButton}>
+          <Text>Tap</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: colors.secondary,
+    paddingVertical: 20,
+    paddingHorizontal: 14,
+    borderRadius: 10,
+  },
+  mainPartContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 160,
+    width: 160,
+  },
+  optionColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  optionButton: {
+    height: 40,
+    width: 80,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 7,
+    padding: 4,
+  },
+});
 
 export default Metronome;
 
