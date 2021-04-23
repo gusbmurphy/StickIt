ddf76b18e8c4292308a92a484bb21ebdd2e50b93
Augustus Murphy <gusbmurphy@gmail.com>
2021-04-23 10:44:20 -0400

    Update exports from styles' index

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,6 @@
 import {buttonStyles} from './buttons';
+import colors from './colors';
+import fontSizes from './font-sizes';
+import shadows from './shadows';
 
-export {buttonStyles};
+export {buttonStyles, colors, fontSizes, shadows};
