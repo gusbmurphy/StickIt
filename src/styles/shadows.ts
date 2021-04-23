16d32901ee0d0ef1f5c8a27a60e979240ed17f86
Augustus Murphy <gusbmurphy@gmail.com>
2021-04-23 10:44:04 -0400

    Create shadows global styles

---
+++ src/styles/shadows.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const shadows = StyleSheet.create({
+  default: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+  },
+});
+
+export default shadows;
