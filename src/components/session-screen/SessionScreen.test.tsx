069a107e971cd7f16ccb7cf7974ecfcd887ca751
Augustus Murphy <gusbmurphy@gmail.com>
2021-04-26 09:37:00 -0400

    Several updates to SessionScreen test, including 'boarding up'  bad mocking tests

--- src/components/session-screen/SessionScreen.test.tsx
+++ src/components/session-screen/SessionScreen.test.tsx
@@ -4,15 +4,13 @@ import SessionScreen, {
   exerciseNameA11yLabel,
   exerciseGroupA11yLabel,
   exerciseInfoButtonA11yLabel,
+  nextButtonA11yLabel,
+  addNoteButtonA11yLabel,
 } from '.';
 import {generateSession} from '../../util/generate-data';
 import {clockTestId} from './ExerciseClock';
 import {metronomeTestId} from './Metronome';
-import {NavigationContainer} from '@react-navigation/native';
 import {ExerciseSession} from '../../types';
-import {createStackNavigator} from '@react-navigation/stack';
-import {RootStackParamList} from '../../../App';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function createSessionScreenProps(
   session: ExerciseSession,
@@ -27,6 +25,25 @@ function createSessionScreenProps(
     },
   };
 }
+
+// TODO: This mocking works well enough for a single test, but we need to be able
+// to change the funcitonality between tests. I think that the 'ts-jest' library's
+// helper funcitons could be useful here!
+
+// jest.mock('./ExerciseClock.tsx', () => {
+//   // eslint-disable-next-line no-shadow
+// //  const React = require('react');
+//   const {Text} = require('react-native');
+//   const {useEffect} = require('react');
+//   const RealClock = jest.requireActual('./ExerciseClock.tsx');
+//   return ({onFinish}: React.ComponentProps<typeof RealClock>) => {
+//     useEffect(() => {
+//       console.log('Hello!');
+//       onFinish!();
+//     });
+//     return <Text>Clock Mock</Text>;
+//   };
+// });
 
 describe('Session Screen', () => {
   test("the current exercise's name and focus group is displayed", () => {
@@ -75,17 +92,58 @@ describe('Session Screen', () => {
     expect(queryByTestId(metronomeTestId)).toBeTruthy();
   });
 
-  test.todo(
-    'when the exercise has finished, an "Add Note" button is displayed',
-  );
-  test.todo(
-    'the "Add Note" button is not displayed before the exercise has finished',
-  );
+  //  TODO: These tests need the ExerciseClock to be mocked so that it just calls it's
+  //  "onFinish" function without the need to mess with any time-related functionality.
+  describe('Tests With Completed Clock', () => {
+    test.todo(
+      'when the exercise has finished, an "Add Note" button is displayed',
+      () => {
+        // const ClockMock = ({
+        //   onFinish,
+        // }: React.ComponentProps<typeof ExerciseClock>) => {
+        //   useEffect(() => onFinish!());
+        //   return <Text>Clock Mock</Text>;
+        // };
+        // jest.doMock('./ExerciseClock', () => ClockMock);
+        // const session = generateSession();
+        // const {getByA11yLabel} = render(
+        //   <SessionScreen {...createSessionScreenProps(session)} />,
+        // );
+        // expect(getByA11yLabel(addNoteButtonA11yLabel)).toBeTruthy();
+      },
+    );
+
+    test.todo(
+      'when the exercise is finished, a "Next" button is displayed',
+      () => {
+        // const session = generateSession();
+        // const {getByA11yLabel} = render(
+        //   <SessionScreen {...createSessionScreenProps(session)} />,
+        // );
+        // expect(getByA11yLabel(nextButtonA11yLabel)).toBeTruthy();
+      },
+    );
+  });
+
+  test('the "Add Note" button is not displayed before the exercise has finished', () => {
+    const session = generateSession();
+    const {queryByA11yLabel} = render(
+      <SessionScreen {...createSessionScreenProps(session)} />,
+    );
+
+    expect(queryByA11yLabel(addNoteButtonA11yLabel)).toBeFalsy();
+  });
+
   test.todo('pressing the "Add Note" button brings up a modal to add a note');
-  test.todo('when the exercise is finished, a "Next" button is displayed');
-  test.todo(
-    'the "Next" button is not displayed before the exercise is finished',
-  );
+
+  test('the "Next" button is not displayed before the exercise is finished', () => {
+    const session = generateSession();
+    const {queryByA11yLabel} = render(
+      <SessionScreen {...createSessionScreenProps(session)} />,
+    );
+    expect(queryByA11yLabel(nextButtonA11yLabel)).toBeFalsy();
+  });
+
   test.todo('pressing the "Next" button navigates to the next exercise');
   test.todo(
     'there are progress pips showing the correct number of exercises in the session, and the correct current exercise',
