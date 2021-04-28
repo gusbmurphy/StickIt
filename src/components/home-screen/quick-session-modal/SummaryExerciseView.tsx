0034583f04dc0b5681edbb3535d2e3f2f67dbd5d
Augustus Murphy <gusbmurphy@gmail.com>
2021-04-28 09:01:52 -0400

    Update SummaryExerciseView.tsx

--- src/components/home-screen/quick-session-modal/SummaryExerciseView.tsx
+++ src/components/home-screen/quick-session-modal/SummaryExerciseView.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import Text from '../../../styles/components/StyledText';
 import {Exercise} from '../../../types';
 import colors from '../../../styles/colors';
+import InfoIcon from '../../../assets/info.svg';
+import RedoIcon from '../../../assets/redo-alt.svg';
 import {
   summaryExerciseTestId,
   quickSessionSummaryExerciseA11yLabel,
   summaryExerciseDurationTestId,
 } from './GeneratedSessionSummaryView';
+import {fontSizes} from '../../../styles';
 
 export const SummaryExerciseView = (props: {
   exercise: Exercise;
@@ -20,30 +24,33 @@ export const SummaryExerciseView = (props: {
       props.exercise.name,
       props.numberOfMinutes,
     )}
-    style={styles.mainContainer}>
+    style={styles.infoContainer}>
     <InfoButton />
     <View style={styles.nameDurationContainer}>
       <View>
         <Text style={styles.groupText}>{props.groupName}</Text>
-        <Text style={styles.largeText}>{props.exercise.name}</Text>
+        <Text style={styles.exerciseName}>{props.exercise.name}</Text>
       </View>
-      <Text style={styles.largeText} testID={summaryExerciseDurationTestId}>
+      <Text style={styles.durationText} testID={summaryExerciseDurationTestId}>
         {props.numberOfMinutes}
       </Text>
     </View>
   </View>
 );
 
-const InfoButton = () => <Text style={styles.infoButton}>I</Text>;
+const InfoButton = () => <InfoIcon style={styles.infoButton} width={25} />;
 
 const styles = StyleSheet.create({
   mainContainer: {
+    flex: 1,
+  },
+  infoContainer: {
     backgroundColor: colors.secondary,
     borderRadius: 10,
     flexDirection: 'row',
     alignItems: 'center',
-    marginVertical: 8,
     paddingVertical: 8,
+    marginVertical: 8,
   },
   nameDurationContainer: {
     flex: 1,
@@ -52,11 +59,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingRight: 12,
   },
-  largeText: {
-    fontSize: 18,
+  exerciseName: {
+    fontSize: fontSizes.medium,
   },
   groupText: {
-    fontSize: 12,
+    fontSize: fontSizes.small,
+  },
+  durationText: {
+    fontSize: fontSizes.medium,
+    marginRight: 10,
   },
   infoButton: {
     marginHorizontal: 8,
